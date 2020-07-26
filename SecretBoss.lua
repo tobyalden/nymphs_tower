c5ed620bccce6cfa1b65db56a48df465a4d04d77
Toby Alden <tobyalden@Tobys-MacBook-Pro.local>
2020-07-26 14:06:26 -0700

    Add missing files to repo. Begin work on SecretBoss second phase

---
+++ SecretBoss.lua
@@ -0,0 +1,230 @@
+SecretBoss = class("SecretBoss", Entity)
+SecretBoss:include(Boss)
+
+SecretBoss.static.MAX_SPEED = 200
+SecretBoss.static.CHASE_SPEED = 50
+SecretBoss.static.PAUSE_TIME = 0.5
+--SecretBoss.static.MAX_SPEED = 0
+
+function SecretBoss:initialize(x, y, nodes)
+    Entity.initialize(self, x, y)
+    self.displayName = "NYMPH"
+    self.flag = "secret_boss"
+    self.types = {"enemy"}
+    self.startingHealth = 20
+    --self.startingHealth = 1
+    self.health = self.startingHealth
+    self.graphic = Sprite:new("secretboss.png")
+    self.mask = Hitbox:new(self, 40, 50)
+    self.layer = 0
+    self.nodes = {}
+    for i, node in pairs(nodes) do
+        self.nodes[i] = Vector:new(node.x, node.y)
+    end
+    self.nodeIndex = 1
+    self.reversed = love.math.random() > 0.5
+    self.pauseTime = SecretBoss.PAUSE_TIME
+    self.pauseTimer = self:addTween(Alarm:new(self.pauseTime))
+    self.attackTimer = self:addTween(Alarm:new(self.pauseTime / 2, function()
+        self:fireBullet()
+    end))
+    self.fanTimer = self:addTween(Alarm:new(1, function()
+        self:fireFan()
+    end, "looping"))
+    self.phaseNumber = 2
+end 
+
+function SecretBoss:update(dt)
+    self:bossUpdate(dt)
+    Entity.update(self, dt)
+end
+
+function SecretBoss:fireBullet()
+    -- TODO: This is gonna break when i move the nodes
+    if self.y == 7864 then
+        self:fireSpread()
+    else
+        self:fireDropShot()
+    end
+end
+
+function SecretBoss:fireFan()
+    local numBullets = 8
+    local increment = (math.pi * 2) / numBullets
+    for i = 1, numBullets do
+        local rotation = increment * (i - 1)
+        local bulletAngle = Vector:new(math.cos(rotation), math.sin(rotation))
+        local bullet = EnemyBullet:new(
+            self,
+            self.x + self.mask.width / 2 - 9,
+            self.y + self.mask.height / 2 - 9,
+            bulletAngle,
+            EnemyBullet.BULLET_SPEED * 1.5,
+            false, true
+        )
+        self.world:add(bullet)
+    end
+end
+
+function SecretBoss:fireDropShot()
+    local towardsPlayer = Vector:new(
+        self.world.player:getMaskCenter().x - self:getMaskCenter().x,
+        self.world.player:getMaskCenter().y - self:getMaskCenter().y
+    )
+    towardsPlayer:normalize()
+    local shotAngle = Vector:new(towardsPlayer.x, -1)
+    local shotSeparation = 30
+    local bullet = EnemyBullet:new(
+        self,
+        self.x,
+        self.y,
+        shotAngle,
+        150 + love.math.random() * 20, true
+    )
+    self.world:add(bullet)
+end
+
+function SecretBoss:fireSpread()
+    local towardsPlayer = Vector:new(
+        self.world.player:getMaskCenter().x - self:getMaskCenter().x,
+        self.world.player:getMaskCenter().y - self:getMaskCenter().y
+    )
+    local bullet = EnemyBullet:new(
+        self,
+        self.x + self.mask.width / 2 - 9,
+        self.y + self.mask.height / 2 - 9,
+        towardsPlayer,
+        EnemyBullet.BULLET_SPEED * 1.5,
+        false, true
+    )
+    self.world:add(bullet)
+
+    local spreadAmount = 0.2
+
+    local sideBullet1Heading = towardsPlayer:copy()
+    sideBullet1Heading:rotate(spreadAmount)
+    local sideBullet1 = EnemyBullet:new(
+        self,
+        self.x + self.mask.width / 2 - 9,
+        self.y + self.mask.height / 2 - 9,
+        sideBullet1Heading,
+        EnemyBullet.BULLET_SPEED * 1.5,
+        false, true
+    )
+    self.world:add(sideBullet1)
+
+    local sideBullet2Heading = towardsPlayer:copy()
+    sideBullet2Heading:rotate(-spreadAmount)
+    local sideBullet2 = EnemyBullet:new(
+        self,
+        self.x + self.mask.width / 2 - 9,
+        self.y + self.mask.height / 2 - 9,
+        sideBullet2Heading,
+        EnemyBullet.BULLET_SPEED * 1.5,
+        false, true
+    )
+    self.world:add(sideBullet2)
+end
+
+function SecretBoss:movement(dt)
+    if self.phaseNumber == 1 then
+        self:phaseOneMovement(dt)
+    else
+        if self.world:hasFlag(self.flag) and not self.fanTimer.active then
+            self.fanTimer:start()
+        end
+        self:phaseTwoMovement(dt)
+    end
+end
+
+function SecretBoss:phaseTwoMovement(dt)
+    local towardsPlayer = Vector:new(
+        self.world.player:getMaskCenter().x - self:getMaskCenter().x,
+        self.world.player:getMaskCenter().y - self:getMaskCenter().y
+    )
+    local maxSpeed = SecretBoss.CHASE_SPEED
+    if not self.world.isHardMode then
+        maxSpeed = maxSpeed / 2
+    end
+    towardsPlayer:normalize(maxSpeed)
+    self:moveBy(towardsPlayer.x * dt, towardsPlayer.y * dt)
+end
+
+function SecretBoss:phaseOneMovement(dt)
+    if self.pauseTimer.active then
+        return
+    end
+    --if self.world:hasFlag(self.flag) and not self.lungeTimer.active then
+        --self.lungeTimer:start(lungeTime)
+    --end
+    --print(self.lungeTimer.time)
+
+    local maxSpeed = SecretBoss.MAX_SPEED
+    if not self.world.isHardMode then
+        maxSpeed = maxSpeed / 2
+    end
+    local moveAmount = maxSpeed * dt
+    local reversed = self.reversed
+
+    while moveAmount > 0 do
+        local targetNodeIndex = self.nodeIndex
+        if reversed then
+            targetNodeIndex = self.nodeIndex - 1
+            if not self.nodes[targetNodeIndex] then
+                targetNodeIndex = #self.nodes
+            end
+        end
+        local towardsNode = Vector:new(
+            self.nodes[targetNodeIndex].x - self.x,
+            self.nodes[targetNodeIndex].y - self.y
+        )
+        local distanceToNextNode = towardsNode:len()
+        if distanceToNextNode > moveAmount then
+            towardsNode:normalize(moveAmount)
+            self:moveBy(
+                towardsNode.x,
+                towardsNode.y
+            )
+            moveAmount = 0
+        else
+            local moveRemainder = Vector:new(
+                self._moveX, self._moveY
+            )
+            self:moveTo(
+                self.nodes[targetNodeIndex].x,
+                self.nodes[targetNodeIndex].y
+            )
+            --if self.phaseNumber == 1 then
+            moveAmount = 0
+            self.pauseTimer:start()
+            self.attackTimer:start()
+            --else
+                --moveAmount = moveAmount - distanceToNextNode + moveRemainder:len()
+            --end
+            if reversed then
+                self.nodeIndex = self.nodeIndex - 1
+                if not self.nodes[self.nodeIndex] then
+                    self.nodeIndex = #self.nodes
+                end
+            else
+                self.nodeIndex = self.nodeIndex + 1
+                if not self.nodes[self.nodeIndex] then
+                    self.nodeIndex = 1
+                end
+            end
+        end
+    end
+end
+
+function SecretBoss:collisions(dt)
+    self:bossCollisions(dt)
+end
+
+function SecretBoss:takeHit(damage)
+    self:bossTakeHit(damage)
+end
+
+function SecretBoss:die()
+    self:bossDie()
+end
+
